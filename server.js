5c91049a45c5734545bb9c2142d42a014c8523fe
Rickson Bozar <rbozar@myseneca.ca>
2025-12-05 19:44:03 -0500

    updated to serverless to run on vercel

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const userService = require("./user-service.js");
 app.use(express.json());
 app.use(cors());
 
-
 const HTTP_PORT = process.env.PORT || 8080;
 
 // JSON Web Token Setup
@@ -24,19 +23,15 @@ let jwtOptions = {
   secretOrKey: process.env.JWT_SECRET
 };
 
-
-
 let strategy = new JwtStrategy(jwtOptions, function (jwt_payload, next) {
   console.log('payload received', jwt_payload);
-
   if (jwt_payload) {
     // The following will ensure that all routes using
-    // passport.authenticate have a req.user._id, req.user.userName, req.user.fullName & req.user.role values
+    // passport.authenticate have a req.user._id, req.user.userName values
     // that matches the request payload data
     next(null, {
       _id: jwt_payload._id,
       userName: jwt_payload.userName,
-   
     });
   } else {
     next(null, false);
@@ -49,78 +44,65 @@ passport.use(strategy);
 // add passport as application-level middleware
 app.use(passport.initialize());
 
-
-app.post("/api/user/register", (req, res) => {
-  userService
-    .registerUser(req.body)
-    .then((msg) => {
-      res.json({ message: msg });
-    })
-    .catch((msg) => {
-      res.status(422).json({ message: msg });
-    });
+app.post("/api/user/register", async (req, res) => {
+  try {
+    await userService.connect();
+    const msg = await userService.registerUser(req.body);
+    res.json({ message: msg });
+  } catch (msg) {
+    res.status(422).json({ message: msg });
+  }
 });
 
-app.post("/api/user/login", (req, res) => {
-  userService
-    .checkUser(req.body)
-    .then((user) => {
-      let payload = {
-        _id: user._id,
-        userName: user.userName,
-       
-      };
-
-      let token = jwt.sign(payload, process.env.JWT_SECRET );
-
-      res.json({ message: "login successful", token: token});
-    })
-    .catch((msg) => {
-      res.status(422).json({ message: msg });
-    });
+app.post("/api/user/login", async (req, res) => {
+  try {
+    await userService.connect();
+    const user = await userService.checkUser(req.body);
+    let payload = {
+      _id: user._id,
+      userName: user.userName,
+    };
+    let token = jwt.sign(payload, process.env.JWT_SECRET);
+    res.json({ message: "login successful", token: token });
+  } catch (msg) {
+    res.status(422).json({ message: msg });
+  }
 });
 
-app.get("/api/user/favourites", passport.authenticate('jwt', { session: false }), (req, res) => {
-  userService
-    .getFavourites(req.user._id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
+app.get("/api/user/favourites", passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    await userService.connect();
+    const data = await userService.getFavourites(req.user._id);
+    res.json(data);
+  } catch (msg) {
+    res.status(422).json({ error: msg });
+  }
 });
 
-app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
-  userService
-    .addFavourite(req.user._id, req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
+app.put("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    await userService.connect();
+    const data = await userService.addFavourite(req.user._id, req.params.id);
+    res.json(data);
+  } catch (msg) {
+    res.status(422).json({ error: msg });
+  }
 });
 
-app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), (req, res) => {
-  userService
-    .removeFavourite(req.user._id, req.params.id)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((msg) => {
-      res.status(422).json({ error: msg });
-    });
+app.delete("/api/user/favourites/:id", passport.authenticate('jwt', { session: false }), async (req, res) => {
+  try {
+    await userService.connect();
+    const data = await userService.removeFavourite(req.user._id, req.params.id);
+    res.json(data);
+  } catch (msg) {
+    res.status(422).json({ error: msg });
+  }
 });
 
-userService
-  .connect()
-  .then(() => {
-    app.listen(HTTP_PORT, () => {
-      console.log("API listening on: " + HTTP_PORT);
-    });
-  })
-  .catch((err) => {
-    console.log("unable to start the server: " + err);
-    process.exit();
-  });
+// Initialize connection on cold start
+userService.connect().catch(err => {
+  console.log("DB connection warning: " + err);
+});
+
+// Export the app for Vercel serverless
+module.exports = app;
